34adeee0cc8cd83429ddc31f5a372c2ef295a69f
Brooks1974 <brooksrichardm@gmail.com>
2024-05-10 21:08:25 +0100

    changed text

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,9 +8,9 @@ os.environ["SERPER_API_KEY"] = st.secrets["SERPER_API_KEY"]
 
 with st.sidebar:
     st.header('Enter Newsletter Details Here')
-    subject = st.text_input("What's the subject?")
+    subject = st.text_input("What does the customer sell?")
     customer = st.text_area("Who is the customer or persona?")
-    additional_info = st.text_area("Any additional information -eg industry or region-")
+    additional_info = st.text_area("What are they interested in buying from you?")
 
 if st.button('Run Research'):
     if not customer or not subject or not additional_info:
