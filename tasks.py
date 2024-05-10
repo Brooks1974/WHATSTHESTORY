a68733f1f6d52d57634a442a2a0f072181436b17
Brooks1974 <brooksrichardm@gmail.com>
2024-05-10 21:05:09 +0100

    fixing

--- tasks.py
+++ tasks.py
@@ -9,7 +9,7 @@ class ResearchCrewTasks:
           agent=agent,
           description=f"Systematically gather and document current and relevant news and articles from diverse sources about {inputs}. Use all available digital tools to ensure comprehensive coverage.",
           expected_output=f"""
-  Detailed Research Report on {inputs}
+          Detailed Research Report on {inputs} to include headline, snippet and url or cite of original article. This is going to be used as newsletter content so we want clear and diverse information on {inputs}
           """
       )
 
@@ -20,12 +20,14 @@ class ResearchCrewTasks:
         context=context,
         description="Critically assess the accuracy, relevance, and depth of the information collected. Employ advanced data analysis methodologies to enhance the information's value, ensuring it meets the high standards required for expert assessment.",
         expected_output=f"""
-        Break each story up like this. 
-        ###
+        Break each story up like this. We want at least 10 stories. 
+
+        *-*-*-*-*-*
         Title/Headline 1
         Brief snippet or summary of the article, focusing on the key points.
         URL: Insert the URL of the original article or cite here
-        ###
+        *-*-*-*-*-*
+
           """
     )
 
@@ -34,14 +36,15 @@ class ResearchCrewTasks:
         return Task(
             agent=agent,
             context=context,
-            description="Synthesize the information provided by the Researcher and enhanced by the Analyst into a compelling, clear, and well-structured summary. Include key findings and appropriately cite all sources to ensure credibility and traceability.",
+            description="Rewrite the 10 pieces of information provided by the Researcher and enhanced by the Analyst into a compelling, clear, and well-structured summary. Include key findings and appropriately cite all sources to ensure credibility and traceability. Apply just a small amount of wit.",
             expected_output=f"""
-        Output as this
-        ###
+        
+        *-*-*-*-*-*
         Title/Headline 1
         Brief snippet or summary of the article, focusing on the key points.
         URL: Insert the URL of the original article or cite here
-        ###
+        *-*-*-*-*-*
+
             """
         )
 
