fb6db4ab4daf4c3759e7cef50659ebba76e3ef83
Brooks1974 <brooksrichardm@gmail.com>
2024-05-10 21:31:57 +0100

    simplified tasks

--- tasks.py
+++ tasks.py
@@ -7,9 +7,9 @@ class ResearchCrewTasks:
     def research_task(self, agent, inputs):
       return Task(
           agent=agent,
-          description=f"Gather and document 10 different current news stories, articles and papers from diverserse sources about {inputs}.",
+          description=f"Gather and document 10 different current news stories from diverserse sources about everything in {inputs}.",
           expected_output=f"""
-          Summary of 10 different news stories to include headline, snippet and url or cite of original article. This is going to be used as newsletter content so we want clear and diverse information on {inputs}
+          Summary of 10 different news stories to include headline, snippet and url or cite of original article. The stories must be relevant to everything in {inputs}
           """
       )
 
@@ -35,7 +35,7 @@ class ResearchCrewTasks:
         return Task(
             agent=agent,
             context=context,
-            description="Rewrite the 10 pieces of information provided by the Researcher and enhanced by the Analyst into a compelling, clear, and well-structured summary. Include key findings and appropriately cite all sources to ensure credibility and traceability. Apply just a small amount of wit.",
+            description="In needed Rewrite the 10 pieces of information into a compelling, clear, and well-structured summary. Apply just a small amount of wit.",
             expected_output=f"""
         
         Present 10 stories in this format. 
